1a9c37e85ba82813bfb5db0bfa22fdf493cbb051
Yaseen <fantasticyaseenshariff@gmail.com>
2022-10-06 00:49:45 +0530

    try merging 1

--- handler.js
+++ handler.js
@@ -1,15 +1,50 @@
-const {listOfFiles, listOfPaths} = require('./filesPaths');
+// const {listOfFiles, listOfPaths} = require('./filesPaths');
 // const quotes = require('./apis/api.json');
 const express = require("express");
 const router = express.Router();
 
-const quotes = require(listOfFiles[0]);
-console.log(quotes);
+// const quotes = require(listOfFiles[0]);
+// console.log(quotes);
 
 // for(let i=0; i<=listOfFiles.length-1;i++){
 //   console.log(listOfFiles[i]);
 // }
 // console.log(listOfFiles);
+var walkSync = function(dir, filelist) {
+  var fs = fs || require('fs'),
+      files = fs.readdirSync(dir);
+  filelist = filelist || [];
+  files.forEach(function(file) {
+    if (fs.statSync(dir + file).isDirectory()) {
+      filelist = walkSync(dir + file + '/', filelist);
+    }
+    else {
+      filelist.push(dir+file);
+    }
+  });
+  return filelist;
+};
+listOfFiles = walkSync("./apis/");
+
+var allFilesPath = function(dirPath, pathList) {
+  var fs = fs || require('fs'),
+      files2 = fs.readdirSync(dirPath);
+  pathList = pathList || [];
+  files2.forEach(function(file2) {
+    if (fs.statSync(dirPath + file2).isDirectory()) {
+      pathList = allFilesPath(dirPath + file2 + '/', pathList);
+    }
+    else {
+      pathList.push(dirPath);
+    }
+  });
+  return pathList;
+};
+listOfPaths = allFilesPath("./apis/");
+
+// ========================================
+
+const quotes = require(listOfFiles[0]);
 
 const randomQuote = () => {
   return quotes[Math.floor(Math.random() * quotes.length)];

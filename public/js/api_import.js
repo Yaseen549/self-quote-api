eeba47453bdaf749dd7a26751775e8ca065e214f
Yaseen <fantasticyaseenshariff@gmail.com>
2022-10-07 15:04:35 +0530

    Add username via api

--- public/js/api_import.js
+++ public/js/api_import.js
@@ -13,14 +13,18 @@
 
 
 // package version
-const loadAPIDetails = async () => {
+const loadPackageDetails = async () => {
   try {
     const res = await fetch("https://selfqa-e2.vercel.app/api/quotes");
     packageDetails = await res.json();
-    console.log(packageDetails[0]);
-    $(".packageVersion").text(packageDetails[0]);
+    // for(var i = 0; i<= packageDetails.length-1;i++){
+    //   console.log(packageDetails[i].id);
+    // }
+    var packageVersion = packageDetails[0].username;
+    $(".packageVersion").text(packageVersion);
+
   } catch (err) {
     console.error(err);
   }
 };
-loadAPIDetails();
+loadPackageDetails();
---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+// exports.handler = async (event) => {
+//     // TODO implement
+//     const response = {
+//         statusCode: 200,
+//                 headers: {
+//             "Access-Control-Allow-Headers" : "Content-Type",
+//             "Access-Control-Allow-Origin": "*",
+//             "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+//         },
+//         body: JSON.stringify('Hello from Lambda!'),
+//     };
+//     return response;
+// };

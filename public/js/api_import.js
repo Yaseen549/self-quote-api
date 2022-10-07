7202c939c0e8df2092a8d130c599b17feffff72e
Yaseen <fantasticyaseenshariff@gmail.com>
2022-10-07 17:53:24 +0530

    apis

--- public/js/api_import.js
+++ public/js/api_import.js
@@ -1,7 +1,7 @@
 // package version
 const loadPackageDetails = async () => {
   try {
-    const res = await fetch("https://selfqa-e2.vercel.app/api/quotes",);
+    const res = await fetch("https://selfqa-e2.vercel.app/apis/quotes",);
     packageDetails = await res.json();
     // for(var i = 0; i<= packageDetails.length-1;i++){
     //   console.log(packageDetails[i].id);

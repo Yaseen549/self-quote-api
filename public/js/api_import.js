319640c81cfa976db531d8f3016ee3e9c4c12e83
Yaseen <fantasticyaseenshariff@gmail.com>
2022-10-07 17:16:57 +0530

    try inbuild js

--- public/js/api_import.js
+++ public/js/api_import.js
@@ -1,30 +1,35 @@
 
+// https://selfqa-e2.vercel.app/api/quotes
 
 
 
+// let url = 'https://selfqa-e2.vercel.app/api/quotes';
+async function load() {
+  const userFiles = require(`./apis/api.json`);
+    // let url = 'https://selfqa-e2.vercel.app/api/quotes';
+    let obj = await (await fetch(userFiles)).json();
+    // console.log(obj);
+}
 
-
-
-
-
+load();
 
 
 
 
 
 // package version
-const loadPackageDetails = async () => {
-  try {
-    const res = await fetch("https://selfqa-e2.vercel.app/api/quotes");
-    packageDetails = await res.json();
-    // for(var i = 0; i<= packageDetails.length-1;i++){
-    //   console.log(packageDetails[i].id);
-    // }
-    var packageVersion = packageDetails[0].username;
-    $(".packageVersion").text(packageVersion);
-
-  } catch (err) {
-    console.error(err);
-  }
-};
-loadPackageDetails();
+// const loadPackageDetails = async () => {
+//   try {
+//     const res = await fetch("https://selfqa-e2.vercel.app/api/quotes");
+//     packageDetails = await res.json();
+//     // for(var i = 0; i<= packageDetails.length-1;i++){
+//     //   console.log(packageDetails[i].id);
+//     // }
+//     var packageVersion = packageDetails[0].username;
+//     $(".packageVersion").text(packageVersion);
+//
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+// loadPackageDetails();

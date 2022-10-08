9e5093cfb1a94005b29e8646757fbe76818be9f5
Yaseen <fantasticyaseenshariff@gmail.com>
2022-10-08 17:18:11 +0530

    try domain

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,9 @@
 // package version
 const loadPackageDetails = async () => {
   try {
-    const res = await fetch("https://selfqa-e2.vercel.app/apis/quotes");
+    // const res = await fetch(`https://localhost:3000/apis/quotes`);
+    var domain = `selfqa-e2.vercel.app`;
+    const res = await fetch(`https://${domain}/apis/quotes`);
     // const res = await fetch(`https://${process.env.VERCEL_URL}/apis/quotes`);
     packageDetails = await res.json();
     packageVersion = packageDetails[0].type;
